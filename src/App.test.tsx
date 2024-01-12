67816d95faa1eb315d99ca8ba2e24a7ad3f45463
Zoosewu <zoosewup@gmail.com>
2024-01-12 21:40:22 +0800

    remove wrong test
    Signed-off-by: Zoosewu <zoosewup@gmail.com>

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,7 +7,6 @@ test('renders learn react link', async () => {
   const { container } = render(<App />)
   const linkElement = container.querySelector('div.App')
   expect(linkElement).toBeInTheDocument()
-  expect(screen.getByText(/React SPA Template/i)).toBeInTheDocument()
   const user = userEvent.setup()
   await user.click(screen.getByText(/reduce/i))
   expect(screen.getByText(/reduce/i)).toBeInTheDocument()
